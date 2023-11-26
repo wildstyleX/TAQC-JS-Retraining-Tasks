0e511fc12b879a4be00599b39e7662688bebf9fc
Artem Dubenko <mysterx5054@gmail.com>
2023-11-26 11:58:54 +0200

    change function to variable, change naming

--- introduction/array_numbers.js
+++ introduction/array_numbers.js
@@ -1,15 +1,15 @@
-function filterNumbers(numsArr) {
-    return numsArr.filter(num => {
+const primeNumbers = numsArr => {
+    return numsArr.filter(number => {
         return (
-            num % 3 === 0 ||
-            num % 5 === 0 ||
-            num % 7 === 0 ||
-            num % 11 === 0 ||
-            num % 13 === 0
+            number % 3 === 0 ||
+            number % 5 === 0 ||
+            number % 7 === 0 ||
+            number % 11 === 0 ||
+            number % 13 === 0
         );
-    })
-}
+    });
+};
 
 const array_numbers = [1, 3, 5, 7, 13, 16, 21, 54, 77, 111, 143, 205];
-const resultArray = filterNumbers(array_numbers);
+const resultArray = primeNumbers(array_numbers);
 console.log(resultArray);
